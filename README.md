52b25ce71cd6fa78aa63d1c510b33374ff0bfdee
AbdelliNasredine <abdou.nasredine@gmail.com>
2020-12-24 23:18:03 +0100

    intial commit -m

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bluetooth connectivity in android
+
+first lab of IOT uni course
+
+### progress
+
+- [ ] turn on bluetooth if it is available in device
+  - [ ] enable bluetooth
+  - [ ] make its discoverable
+- [ ] scan for other devices
---
+++ app/src/main/java/dz/utmb/iot/bl_android/MainActivity.java
@@ -0,0 +1,16 @@
+package dz.utmb.iot.bl_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
